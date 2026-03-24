dd88a173e1c33e408b81c9905d5a8e16eb292948
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-24 23:42:57 +0700

    refactor: separate work experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSocialMedia from './utils/HeroSocialMedia'
 import HeroLink from './utils/HeroLink'
 import HeroProfile from './utils/HeroProfile'
+import HeroWorkExperient from './utils/HeroWorkExperient'
 
 function Hero() {
     return (
@@ -13,20 +14,7 @@ function Hero() {
 
             <HeroSocialMedia />
 
-            <div className="grid grid-cols-3 gap-8 mt-10 w-full max-w-lg">
-                <div>
-                    <p className="text-xl font-bold">7</p>
-                    <p className="text-xs text-slate-500 uppercase tracking-wider">Years of work<br />experience</p>
-                </div>
-                <div>
-                    <p className="text-xl font-bold">50+</p>
-                    <p className="text-xs text-slate-500 uppercase tracking-wider">Completed<br />projects</p>
-                </div>
-                <div>
-                    <p className="text-xl font-bold">20+</p>
-                    <p className="text-xs text-slate-500 uppercase tracking-wider">Satisfied<br />customers</p>
-                </div>
-            </div>
+            <HeroWorkExperient />
 
             <HeroLink />
         </div>
---
+++ src/components/utils/HeroWorkExperient.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function HeroWorkExperient() {
+    return (
+        <div className="grid grid-cols-3 gap-8 mt-10 w-full max-w-lg">
+            <div>
+                <p className="text-xl font-bold">7</p>
+                <p className="text-xs text-slate-500 uppercase tracking-wider">Years of work<br />experience</p>
+            </div>
+            <div>
+                <p className="text-xl font-bold">50+</p>
+                <p className="text-xs text-slate-500 uppercase tracking-wider">Completed<br />projects</p>
+            </div>
+            <div>
+                <p className="text-xl font-bold">20+</p>
+                <p className="text-xs text-slate-500 uppercase tracking-wider">Satisfied<br />customers</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroWorkExperient
