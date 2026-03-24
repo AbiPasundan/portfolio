34d44c928e36e5cbe8137bf14f14f9c7c2599ec6
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-25 00:18:03 +0700

    refactor: separate skill section into defferent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // icons
-import { BiSun } from "react-icons/bi";
-import { FaRegMoon } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
-import { BsDownload } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { BsLinkedin } from "react-icons/bs";
-import { BsFacebook } from "react-icons/bs";
-import { AiOutlineGithub } from "react-icons/ai";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { FaReact, FaNodeJs } from "react-icons/fa";
-import { FaGolang } from "react-icons/fa6";
-import { SiSwagger } from "react-icons/si";
-import { DiPostgresql } from "react-icons/di";
-import { SiMxlinux } from "react-icons/si";
-import { FaLinux } from "react-icons/fa";
-import { SiRedux } from "react-icons/si";
-import { SiRedis } from "react-icons/si";
-import { GrDocker } from "react-icons/gr";
-import { SiKde } from "react-icons/si";
-import { TbBrandVscode } from "react-icons/tb";
-import { DiCss3 } from "react-icons/di";
-import { AiOutlineHtml5 } from "react-icons/ai";
-import { FaBootstrap } from "react-icons/fa";
 
 
 // certificate
@@ -33,35 +11,10 @@ import dicoding2 from "@/assets/img/certificate/dicoding2.jpeg"
 import Hero from './components/Hero';
 import ToggleTheme from './components/ToggleTheme';
 import Portfolio from './components/Portfolio';
+import Skill from './components/utils/Skill';
 
 const App = () => {
     const [activeTab, setActiveTab] = useState('portfolio');
-
-    
-
-    const skills = [
-        { name: 'HTML', level: 'Expert', icon: <AiOutlineHtml5 /> },
-        { name: 'CSS', level: 'Expert', icon: <DiCss3 /> },
-        { name: 'Bootstrap', level: 'Expert', icon: <FaBootstrap /> },
-        { name: 'Tailwind', level: 'Expert', icon: <RiTailwindCssFill /> },
-        { name: 'React', level: 'Intermediate', icon: <FaReact /> },
-        { name: 'Node.js', level: 'Intermediate', icon: <FaNodeJs /> },
-
-        { name: 'Go', level: 'Intermediate', icon: <FaGolang /> },
-        { name: 'Swagger', level: 'Intermediate', icon: <SiSwagger /> },
-        { name: 'Postgresql', level: 'Intermediate', icon: <DiPostgresql /> },
-
-        { name: 'Linux', level: 'Intermediate', icon: <FaLinux /> },
-        { name: 'MXLinux', level: 'Intermediate', icon: <SiMxlinux /> },
-        { name: 'Docker', level: 'Intermediate', icon: <GrDocker /> },
-        { name: 'Redis', level: 'Intermediate', icon: <SiRedis /> },
-        { name: 'Redux', level: 'Intermediate', icon: <SiRedux /> },
-
-        { name: 'Vscode', level: 'Intermediate', icon: <TbBrandVscode /> },
-        { name: 'KDE Plasma', level: 'Intermediate', icon: <SiKde /> },
-
-    ];
-
 
     const certificates = [
         {
@@ -128,17 +81,7 @@ const App = () => {
                             <Portfolio />
                         )}
                         {activeTab === 'skills' && (
-                            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-                                {skills.map((skill, index) => (
-                                    <div key={index} className="flex flex-col items-center p-6 bg-white dark:bg-slate-800/50 border border-slate-100 dark:border-slate-700 rounded-3xl transition-all hover:border-yellow-400 dark:hover:border-yellow-400 hover:shadow-lg group">
-                                        <div className="w-12 h-12 mb-3 flex items-center justify-center rounded-2xl bg-slate-50 dark:bg-slate-700 group-hover:bg-yellow-400 transition-colors">
-                                            {skill.icon}
-                                        </div>
-                                        <span className="font-medium text-slate-700 dark:text-slate-200">{skill.name}</span>
-                                        <span className="text-xs text-slate-400 mt-1">{skill.level}</span>
-                                    </div>
-                                ))}
-                            </div>
+                            <Skill />
                         )}
 
                         {activeTab === 'certificate' && (
