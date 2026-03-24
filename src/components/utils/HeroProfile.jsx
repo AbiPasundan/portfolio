fc36869f396db810a9dd473d6446b83ef5637b33
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-25 01:14:02 +0700

    feat: update profile

--- src/components/utils/HeroProfile.jsx
+++ src/components/utils/HeroProfile.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import profile from '@/assets/img/profile.jpeg'
 
 function HeroProfile() {
     return (
         <div className="relative group">
             <div className="w-32 h-32 rounded-full border-4 border-yellow-400 p-1 overflow-hidden">
-                <img src="https://i.pravatar.cc/300" alt="Wildan Nursyamsi" className="w-full h-full object-cover rounded-full grayscale group-hover:grayscale-0 transition-all duration-500" />
+                <img src={profile} alt="Wildan Nursyamsi" className="w-full h-full object-cover rounded-full grayscale group-hover:grayscale-0 transition-all duration-500" />
             </div>
         </div>
     )
