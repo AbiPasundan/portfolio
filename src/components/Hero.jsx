f86f31bf151feb3557ca60bbcd7a6cebe5bbeef2
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-24 23:40:14 +0700

    refactor:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import HeroSocialMedia from './utils/HeroSocialMedia'
 import HeroLink from './utils/HeroLink'
+import HeroProfile from './utils/HeroProfile'
 
 function Hero() {
     return (
         <div className="flex flex-col items-center pt-12 pb-8 px-6 text-center">
-            <div className="relative group">
-                <div className="w-32 h-32 rounded-full border-4 border-yellow-400 p-1 overflow-hidden">
-                    <img src="https://i.pravatar.cc/300" alt="Wildan Nursyamsi" className="w-full h-full object-cover rounded-full grayscale group-hover:grayscale-0 transition-all duration-500" />
-                </div>
-            </div>
+            <HeroProfile />
 
             <h1 className="mt-4 text-3xl font-bold tracking-tight">Wildan Nursyamsi</h1>
             <p className="text-slate-500 dark:text-slate-400 font-medium">Software Engineer</p>
